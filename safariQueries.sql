cefb8843373b69c490f240827c25da22de8328d0
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-05-10 23:41:56 +0100

    Add extentions

--- safariQueries.sql
+++ safariQueries.sql
@@ -15,3 +15,10 @@ ON employees.id = assignments.employee_id
 INNER JOIN enclosures
 ON enclosures.id = assignments.enclosure_id
 WHERE enclosures.id = 1;
+
+-- Extentions 
+-- The names of staff working in enclosures which are closed for maintenance
+-- The name of the enclosure where the oldest animal lives. If there are two animals who are the same age choose the first one alphabetically.
+-- The number of different animal types a given keeper has been assigned to work with.
+-- The number of different keepers who have been assigned to work in a given enclosure
+-- The names of the other animals sharing an enclosure with a given animal (eg. find the names of all the animals sharing the big cat field with Tony)
